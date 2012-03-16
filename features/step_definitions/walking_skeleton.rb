107ce0590cbe2b73c4063127383c0e51153fe57f
Chris Roos & James Adam <pairing+chris.roos+james.adam@gofreerange.com>
2012-03-16 18:31:37 +0000

    A failing test to remind us what we were doing.

--- features/step_definitions/walking_skeleton.rb
+++ features/step_definitions/walking_skeleton.rb
@@ -1,9 +1,18 @@
 Given /^some messages exist on the server$/ do
-  GmailAccount.stubs(:messages).returns([Mail.new("Subject: Message one"), Mail.new("Subject: Message two")])
+  GmailAccount.stubs(:messages).returns([Mail.new("Subject: Message one\nDate: 2012-05-23 12:34:45\nFrom: Dave"),
+                                         Mail.new("Subject: Message two\nDate: 2012-06-22 09:21:31\nFrom: Barry")])
 end
 
 Then /^they should be visible on the messages page$/ do
   visit "/"
-  assert page.has_content?("Message one")
-  assert page.has_content?("Message two")
+  within ".message" do
+    assert page.has_css? ".subject", "Message one"
+    assert page.has_css? ".date", "2012-05-23 12:34:45"
+    assert page.has_css? ".sender", "Dave"
+  end
+  within ".message" do
+    assert page.has_css? ".subject", "Message two"
+    assert page.has_css? ".date", "2012-06-22 09:21:31"
+    assert page.has_css? ".sender", "Barry"
+  end
 end
